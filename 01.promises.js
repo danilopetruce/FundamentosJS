cf46b4766da88c7a30655907438462fa6edac58e
Danilo Petruce <danilopetruce@hotmail.com>
2020-06-15 20:34:16 -0300

    Exercicios finalizados com sucesso

---
+++ 01.promises.js
@@ -0,0 +1,47 @@
+const readline = require("readline");
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// terminal.question("Qual é seu nome? \n", (nome) => {
+//   terminal.question("Qual é seu telefone? \n", (telefone) => {
+//     console.log(`
+
+//         Nome: ${nome},
+//         Telefone: ${telefone}
+
+//         `);
+//     terminal.close();
+//   });
+// });
+
+function questionAsync(texto) {
+  return new Promise((resolve, reject) => {
+    terminal.question(`${texto} \n`, resolve);
+  });
+}
+
+let nome = "";
+let telefone = "";
+
+Promise.resolve()
+  .then(() => questionAsync("Qual é o seu nome?"))
+  .then((respostaNome) => {
+    if (!respostaNome) throw new Error("Campo Vazio");
+    nome = respostaNome;
+  })
+
+  .then(() => questionAsync("Qual é o seu Telefone?"))
+  .then((respostaTelefone) => {
+    if (!respostaTelefone) throw new Error("Campo Vazio");
+
+    telefone = respostaTelefone;
+  })
+  .then(() => {
+    console.log(`Nome: ${nome}, Telefone: ${telefone}`);
+  })
+  .catch((error) => {
+    console.log("DEU RUIM", error.stack);
+  })
+  .finally(() => terminal.close());
